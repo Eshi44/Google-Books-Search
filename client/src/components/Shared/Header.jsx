03f446e539aeaa55bd83dc2e8058a04f9b8f1dfc
Eshi44 <e.hameliervolino@gmail.com>
2020-05-08 12:42:25 -0400

    manifest

--- client/src/components/Shared/Header.jsx
+++ client/src/components/Shared/Header.jsx
@@ -3,19 +3,11 @@ import BooksImage from "../../images/book.jpg";
 import "./Header.css";
 
 function Header() {
-
 	return (
 		<div className="fluid-container">
-	
-				
-				
-					
-						<img id="img" src={BooksImage} alt="Books" />
-					
-				
-			
-			</div>
-    );
+			<img id="img" src={BooksImage} alt="Books" />
+		</div>
+	);
 }
 
 export default Header;
