660180e690d373b21596f3ef22aa669538479190
Eshi44 <e.hameliervolino@gmail.com>
2020-05-08 09:46:29 -0400

    added carousel

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/Shared/NavBar";
 import Footer from "./components/Shared/Footer";
+import HomeSearch from "./containers/HomeSearch";
 
 function App() {
 	return (
 		<>
 			<Router>
 				<NavBar />
+        <Switch>
+          <Route exact path="/" component={HomeSearch} />
+          </Switch>
+
 				<Footer />
 			</Router>
 		</>
---
+++ client/src/containers/HomeSearch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../components/Shared/Carousel";
+
+function HomeSearch() {
+	return (
+		<>
+
+			<Carousel />
+            <h1>Search</h1>
+		</>
+	);
+}
+
+export default HomeSearch;
