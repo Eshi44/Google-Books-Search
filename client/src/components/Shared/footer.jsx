1d23694f1da38fca81dab0d06517b6e34e7b4592
Eshi44 <e.hameliervolino@gmail.com>
2020-05-08 09:01:14 -0400

    added navbar and footer

---
+++ client/src/components/Shared/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<>
+			<footer className="page-footer">
+				<div className="container-fluid text-center">
+					<p>Google Books</p>
+				</div>
+			</footer>
+		</>
+	);
+}
+
+export default Footer;
